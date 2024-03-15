a41a0e3679ce37f4ffd34b7c4b840bf88e9dc058
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2024-03-15 21:11:20 +0530

    Create swapp.py

---
+++ swapp.py
@@ -0,0 +1,21 @@
+#swap using temp variable
+def swap(a,b):
+temp=a
+a=b
+b=temp
+print("after swapping the no. is:",a,b)
+#main func
+a=int(input("enter 1st no.:")) 
+b=int(input("enter 2nd no.:")) 
+print("before swapping the no. is:",a,b)
+swap(a,b)
+
+#swap without using temp variable
+def swap(a,b):
+a,b=b,a
+print("after swapping the no. is:",a,b)
+#main func
+a=int(input("enter 1st no.:")) 
+b=int(input("enter 2nd no.:")) 
+print("before swapping the no. is:",a,b)
+swap(a,b)
